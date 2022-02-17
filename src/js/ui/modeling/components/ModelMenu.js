342714ab21fc0efaa90d3a26a25ec13e21b99749
Daniel Reynolds <danielreynolds101@gmail.com>
2022-02-17 13:43:30 -0700

    change proto

--- src/js/ui/modeling/components/ModelMenu.js
+++ src/js/ui/modeling/components/ModelMenu.js
@@ -267,8 +267,8 @@ export default class ModelMenu extends React.Component {
             ...await this.getExtraRequestParams()
         };
 
-        //console.log(JSON.stringify(q))
-        //console.log(q)
+        console.log(JSON.stringify(q))
+        console.log({q})
         const stream = this._sustainQuerier.executeModelQuery(JSON.stringify(q));
         let resData = [];
         stream.on('data', function (r) {
@@ -278,6 +278,7 @@ export default class ModelMenu extends React.Component {
         }.bind(this));
         stream.on('end', function (end) {
             //console.log("end")
+            console.log({resData})
             this.handleFullResponse(resData);
             this.setState({
                 modelStatus: "built"
